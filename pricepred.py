254a38b6a0c4dc7934bed7cfeebb97b8a6d14173
NagaYasaswini <nagayasaswini2128@gmail.com>
2025-10-17 11:08:22 +0530

    Changed requirements to earlier

--- pricepred.py
+++ pricepred.py
@@ -104,4 +104,4 @@ def predict_api(features: HouseFeatures):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-   uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
